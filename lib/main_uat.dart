fe444deb22799ecbaac1d91205f75935c4200a14
Fahmi Dwi S <dwifhmi.developer@gmail.com>
2025-03-06 14:24:46 +0700

    - setup & initialize SDK firebase

--- lib/main_uat.dart
+++ lib/main_uat.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+
+import 'core/core.dart';
 import 'core/environtments/env_enum.dart';
+import 'firebase_options_uat.dart';
 import 'main_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   mainApp(Environtment.uat);
 }
