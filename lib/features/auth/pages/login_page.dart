ee644577216b8f3218ba5172ef53b55e5cee322a
Fahmi Dwi S <dwifhmi.developer@gmail.com>
2025-03-07 23:29:11 +0700

    setup supabase

--- lib/features/auth/pages/login_page.dart
+++ lib/features/auth/pages/login_page.dart
@@ -1,3 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 import '../../../core/constants/images_assets.dart';
 import '../../../core/core.dart';
 import '../../order/pages/create_order_page.dart';
@@ -7,6 +11,40 @@ class LoginPage extends StatelessWidget {
   static const path = '/login';
 
   const LoginPage({super.key});
+
+  Future<void> signInWithGoogle(BuildContext c) async {
+    try {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      final user = await FirebaseAuth.instance.signInWithCredential(credential);
+      final ok = await checkUser(googleUser?.email ?? "");
+      if (ok) {
+        print(googleUser?.email);
+        c.go(CreateOrderPage.path);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<bool> checkUser(String email) async {
+    final check = await Supabase.instance.client.from('company').select();
+    // .eq('email', email);
+
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,8 +77,8 @@ class LoginPage extends StatelessWidget {
                   SizedBox(height: kSizeM),
                   GradientButton(
                     width: 300,
-                    onPressed: () {
-                      context.go(CreateOrderPage.path);
+                    onPressed: () async {
+                      await signInWithGoogle(context);
                     },
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
